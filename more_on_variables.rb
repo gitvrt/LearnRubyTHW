c9e32db24a9f8ff48199848441af8f90eb8d3930
raviteja.valluri <raviteja.valluri@tcs.com>
2015-01-13 15:28:38 +0530

    more on variables usage

---
+++ more_on_variables.rb
@@ -0,0 +1,9 @@
+days_in_a_week = 7
+weeks_in_a_month = 4
+months_in_a_year = 12
+days_in_a_year = 365
+
+puts "A week consists of #{days_in_a_week} days "
+puts "A month consists of #{weeks_in_a_month} weeks"
+puts "An year consists of #{months_in_a_year} months which is equal to #{days_in_a_year} days"
+puts "#{days_in_a_year/days_in_a_week} weeks present in 1 year (generally)"
