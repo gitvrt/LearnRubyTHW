a85fe640c13ebedadbfa789c1145ba5690fe2f7f
raviteja.valluri <raviteja.valluri@tcs.com>
2015-01-13 15:42:54 +0530

    strings in ruby

---
+++ strings_in_ruby.rb
@@ -0,0 +1,9 @@
+puts "A String is a palindrome if it satisfies : string == string.reverse"
+
+name ="madam"
+
+puts "Is madam a palindrome : #{name==name.reverse}"
+
+lhs= "Thank"
+rhs = " you"
+puts lhs+rhs
