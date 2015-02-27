e16c3445bef8e15887e9f40a97f456370667ce5e
gitvrt <valluriraviteja@gmail.com>
2015-02-27 18:10:56 +0530

    Exercise programs on strings in ruby

---
+++ more_printing.rb
@@ -0,0 +1,15 @@
+puts "Mary has a little lamb."
+puts "Its fleece was white as #{'snow'}"
+puts "And everywhere that mary went"
+puts "."*10
+
+char1 = "R"
+char2 = "a"
+char3 = "v"
+char4 = "i"
+char5 = "t"
+char6 = "e"
+char7 = "j"
+char8 = "a"
+
+puts char1+char2+char3+char4+char5+char6+char7+char8
