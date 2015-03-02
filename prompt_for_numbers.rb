35e4cba75c74ab39757fb707f762fc0b7f362223
gitvrt <valluriraviteja@gmail.com>
2015-03-02 15:07:57 +0530

    Program that adds two numbers

---
+++ prompt_for_numbers.rb
@@ -0,0 +1,9 @@
+puts "****Program that adds two numbers*****"
+
+print "Enter a value : "
+a = gets.chomp.to_i #Converting string to integer
+
+print "Enter b value : "
+b = gets.chomp.to_i #Converting string to integer
+
+puts "Sum of #{a} and #{b} is #{a+b}" 
