1401f0936abe1ed4055d990db356726d9890d705
gitvrt <valluriraviteja@gmail.com>
2015-03-02 12:25:03 +0530

    Program on escape sequences

---
+++ escape-sequences.rb
@@ -0,0 +1,16 @@
+tabby_dog = "\t I'm tabbed in."
+persian_dog = "I'm split\non line"
+backslash_dog = "I'm \\ a \\ dog"
+
+formatted_text ="""
+I'll do a Dogs-list:
+\t*Bull-dog
+\t*Pet-dog
+\t*Street-dog
+
+"""
+
+puts tabby_dog
+puts persian_dog
+puts backslash_dog
+puts formatted_text
