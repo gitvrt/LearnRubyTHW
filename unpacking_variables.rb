39d262aef180c1a3b89b653844d658fd4c67feba
gitvrt <valluriraviteja@gmail.com>
2015-03-02 15:22:29 +0530

    Program to read variables from commandline

---
+++ unpacking_variables.rb
@@ -0,0 +1,6 @@
+#Run this program like : ruby unpacking_variables.rb I Love India
+first, second, third = ARGV
+
+puts "Your First variable is #{first}"
+puts "Your Second variable is #{second}"
+puts "Your Third variable is #{third}"
