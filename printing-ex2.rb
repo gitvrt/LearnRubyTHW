5100c59e35c73351036fde04aa09e0c553734faa
gitvrt <valluriraviteja@gmail.com>
2015-03-02 12:10:51 +0530

    one more printing exercise

---
+++ printing-ex2.rb
@@ -0,0 +1,14 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJanuary\nFebruary\nMarch\nApril\nJune\nJuly\nAugust\nSeptember\nOctober\nNovember\nDecember"
+
+puts "List of days #{days}"
+puts "List of Months #{months}"
+
+puts "\nFollowring is a pre formatted text"
+
+puts """
+I am Ravi
+I am from TCS
+My skills are Java and Ruby
+
+"""
