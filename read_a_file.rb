224b52c70d60cd53907f6c4d1480263b7cd47762
gitvrt <valluriraviteja@gmail.com>
2015-03-11 14:13:32 +0530

    Program that reads file contents and then closes it

--- read_a_file.rb
+++ read_a_file.rb
@@ -4,10 +4,13 @@
 
  puts "here is your #{filename} contents :\n#{file_content.read}"
 
+ file_content.close #closing the file
+
  print "Enter another filename : "
  another_file = open($stdin.gets.chomp)
 
  puts another_file.read
+ another_file.close #closing the file
 
 
 
